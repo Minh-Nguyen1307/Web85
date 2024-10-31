c11f0be90d1bd7e17f7ba4977d0f603c766bec4a
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2024-10-31 10:13:04 +0700

    .

--- server/lesson1.js
+++ server/lesson1.js
@@ -1,25 +1,33 @@
-import http from 'http'
-import { customers, products, orders } from './data.js'
+import http from 'http';
+import { customers } from './data.js';
+
 const application = http.createServer((request, response) => {
     const endpoint = request.url;
     const method = request.method;
+
     switch (endpoint) {
         case '/':
-            response.end (`Hi`);
+            
+            response.end('Hi');
             break;
+
         case '/customers':
             if (method === "GET") {
+                
                 response.end(JSON.stringify(customers));
-            }
-            else {
-                response.end(JSON.stringify(`Error`));
+            } else {
+               
+                response.end(JSON.stringify({ error: 'Method not allowed' }));
             }
             break;
-            default:
-            response.end(`404 Notfound`);
+
+        default:
+           
+            response.end('404 Not Found');
             break;
     }
 });
+
 application.listen(8080, () => {
-    console.log('Server is running!');
+    console.log('Server is running on port 8080!');
 });
