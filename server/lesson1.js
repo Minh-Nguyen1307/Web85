edd252ef17bc1fdb98fa73c8dcf29ba3fb9c7194
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2024-10-31 11:14:25 +0700

    .

--- server/lesson1.js
+++ server/lesson1.js
@@ -5,26 +5,24 @@ const application = http.createServer((request, response) => {
   const endpoint = request.url;
   const method = request.method;
 
-  if (endpoint === '/orders/highvalue' && method === 'GET') {
-    const highValueOrders = orders.filter(order => order.totalPrice > 10000000);
-    
-  
-    response.end(JSON.stringify(highValueOrders));}
-    else {
-        switch (endpoint) {
-            case '/':
-              
-                response.end('Hi');
-                break;
-            case '/customers':
-                if (method === "GET") {
-                    
-                    response.end(JSON.stringify(customers));
-                } else {
-                    
-                    response.end(JSON.stringify({ error: 'Method not allowed' }));
-                }
-                break;
+  if (endpoint === "/orders/highvalue" && method === "GET") {
+    const highValueOrders = orders.filter(
+      (order) => order.totalPrice > 10000000
+    );
+
+    response.end(JSON.stringify(highValueOrders));
+  } else {
+    switch (endpoint) {
+      case "/":
+        response.end("Hi");
+        break;
+      case "/customers":
+        if (method === "GET") {
+          response.end(JSON.stringify(customers));
+        } else {
+          response.end(JSON.stringify({ error: "Method not allowed" }));
+        }
+        break;
       default:
         response.end("404 Not Found");
         break;
